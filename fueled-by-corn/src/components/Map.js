d6fb44e61b45c549b122498d744b8931c3cb4e4a
Adilcs <khimadil@gmail.com>
2021-06-04 19:49:02 -0400

    Added Google Map API
    Need to Create .env.local and add
    REACT_APP_GOOGLE_KEY ="(your API key"

--- fueled-by-corn/src/components/Map.js
+++ fueled-by-corn/src/components/Map.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
 
-const Map = () =>{
+
+function MapObject(){
 return(
-    <h1>Map Component</h1>
+    <div>
+    <h1>GoogleMap</h1>
+    <GoogleMap 
+    defaultZoom={10}
+    defaultCenter={{lat:33.7490, lng: -84.3880}}
+    />
+    </div>
 );
 };
+const WrappedMap = withScriptjs(withGoogleMap(MapObject));
 
 
-
-export default Map;
+export default function Map() {
+    return <div style={{width: '100vw', height: '60vh'}}>
+        <WrappedMap googleMapURL ={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement = {<div style={{ height: "100%"}} />}
+        containerElement = {<div style={{ height: "100%"}} />}
+        mapElement = {<div style={{ height: "100%"}} />}/>
+    </div>
+}
